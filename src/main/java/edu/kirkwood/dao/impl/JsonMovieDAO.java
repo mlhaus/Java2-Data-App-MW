58201018ea156746dc484968149528d53360d588
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-10-22 12:33:29 -0500

    Finished JSON, Oct 15

--- src/main/java/edu/kirkwood/dao/impl/JsonMovieDAO.java
+++ src/main/java/edu/kirkwood/dao/impl/JsonMovieDAO.java
@@ -1,7 +1,9 @@
 package edu.kirkwood.dao.impl;
 
+import com.google.gson.*;
 import edu.kirkwood.dao.MovieDAO;
 import edu.kirkwood.model.Movie;
+import edu.kirkwood.model.json.TmdbMovieResponse;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -42,13 +44,34 @@ public class JsonMovieDAO implements MovieDAO {
     }
 
     /**
+     * Outputs an unformatted json string into a human-readable format
+     * @param json A valid json string
+     */
+    public void prettyPrint(String json) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        JsonElement jsonElement = new JsonParser().parse(json);
+        String formattedJson = gson.toJson(jsonElement);
+        System.out.println(formattedJson);
+    }
+
+    /**
      * Retrieves all movies from the data source that matches the title
      * @param title The movie title a user is searching for
      * @return A List<Movie> movies that matches the search title
      */
     @Override
     public List<Movie> search(String title) {
-        System.out.println(fetchRawData(title));
+        String rawData = fetchRawData(title);
+        Gson gson = new GsonBuilder().create();
+        TmdbMovieResponse movieResponse = null;
+        try {
+            movieResponse = gson.fromJson(rawData, TmdbMovieResponse.class);
+        } catch(JsonSyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        movieResponse.getResults().forEach(System.out::println);
         return List.of();
     }
 }
---
+++ src/main/java/edu/kirkwood/model/json/TmdbMovieResponse.java
@@ -0,0 +1,11 @@
+package edu.kirkwood.model.json;
+
+import java.util.List;
+
+public class TmdbMovieResponse {
+    private List<MovieSearchResult> results;
+
+    public List<MovieSearchResult> getResults() {
+        return results;
+    }
+}
